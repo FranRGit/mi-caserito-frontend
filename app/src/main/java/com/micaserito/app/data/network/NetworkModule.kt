46f50a093f452fb7a9bcbf8afabdfc2a2da79ff1
jacksel <jacksel1074sc@gmail.com>
2025-12-15 00:35:07 -0500

    guardado local

--- app/src/main/java/com/micaserito/app/data/network/NetworkModule.kt
+++ app/src/main/java/com/micaserito/app/data/network/NetworkModule.kt
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object NetworkModule {
 
-    private const val IS_MOCK_MODE = true    // 🔴 CAMBIAR A 'FALSE' CUANDO EL BACKEND ESTÉ LISTO
+    private const val IS_MOCK_MODE = false    // 🔴 CAMBIAR A 'FALSE' CUANDO EL BACKEND ESTÉ LISTO
 
 
     private const val BASE_URL = "http://10.0.2.2:3000/api/v1/"
